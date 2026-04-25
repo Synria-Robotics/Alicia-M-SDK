35ecf8b20d9f57707890eb574645ee39290831cd
EonFB <2282267538@qq.com>
2026-04-25 19:20:03 +0800

    修改例程角度为80度

--- examples/07_demo_move_joint.py
+++ examples/07_demo_move_joint.py
@@ -11,10 +11,8 @@ from robocore.utils.beauty_logger import beauty_print, beauty_print_array
 
 
 # 预设安全关节位置 (度)
-POSITION = [90, -90.0, -90.0, 90.0, 0.0, 0.0]
+POSITION = [90, -90.0, -90.0, 80.0, 0.0, 0.0]
 # POSITION = [0, 0, 0, 0, 0, 20]
-
-
 
 def main():
     beauty_print("Demo: 关节控制 (PV)", type="module")
